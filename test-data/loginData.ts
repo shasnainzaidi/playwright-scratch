ca091416352280327a0d634dc19b14144c19ce78
shasnainzaidi <hasnain.xaidi@outlook.com>
2026-04-02 18:02:44 +0500

    Removing prompts from tracking

--- test-data/loginData.ts
+++
@@ -1,8 +0,0 @@
-export const loginData = {
-    validUser: {
-        email: process.env.USER_EMAIL!,
-        password: process.env.USER_PASSWORD!,
-        phone: process.env.USER_PHONE!,
-        username: 'Everything for “U”'
-    }
-};
